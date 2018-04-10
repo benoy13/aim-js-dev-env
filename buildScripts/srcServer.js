bf062df6b039ded6417132b84e8a85226cbfb753
benoyjohn <benoyjohn@gamestop.com>
2018-04-10 00:04:09 -0500

    moving around

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -10,6 +10,8 @@ const port = 3000;
 const app = express();
 const compiler = webpack(config);
 
+const userRouter = express.Router();
+
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
@@ -19,14 +21,26 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/views/index.html'));
 });
 
-app.get('/users', function(req, res) {
-  // Hard coding for simplicity. Pretend this hits a real database
+userRouter.route('/users')
+
+.get((req,res)=>{
   res.json([
-    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
-    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
-    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+    {"id": 1,"firstName":"Benoy","lastName":"John","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammkkkky","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstName":"Tinjkjkja","lastName":"Lee","email":"lee.tina@hotmail.com"}
   ]);
 });
+
+app.use('/',userRouter);
+
+// app.get('/users', function(req, res) {
+//   // Hard coding for simplicity. Pretend this hits a real database
+//   res.json([
+//     {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+//     {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+//     {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
+//   ]);
+// });
 
 app.listen(port, function(err) {
   if (err) {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import './index.css';
+import './public/css/index.css';
 
-import {getUsers, deleteUser} from './api/userApi';
+import {getUsers, deleteUser} from  './api/userApi';
 
 // Populate table of users via API call.
 getUsers().then(result => {
